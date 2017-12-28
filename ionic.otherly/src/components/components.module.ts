60f320dfd7478d228472036d9f56cf7d179ff761
praveenkumarUI <prvnadhi2000@gmail.com>
2017-12-28 15:32:30 +0530

    Removed all previous soure code

--- ionic.otherly/src/components/components.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { SharePopoverComponent } from './share-popover/share-popover';
-@NgModule({
-	declarations: [SharePopoverComponent],
-	imports: [],
-	exports: [SharePopoverComponent]
-})
-export class ComponentsModule {}
